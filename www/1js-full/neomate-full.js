527efad2422da85d4acbfd1278eeb11b99184d29
Chris Kelly <chris@incubate.co.uk>
2017-03-04 01:52:38 +0000

    Adding unit testing on initialisation

--- www/1js-full/neomate-full.js
+++ www/1js-full/neomate-full.js
@@ -1,8 +1,3 @@
-// Infusions disabled in this version
-// 1. CSS .drugcalc display none
-// 2. "Infusion disable" at the end of the CalculateApp function
-// 3. "INFUSION DISABLE" at the end of this JS function - function disabled
-
 // ################## START: Infusion database #######################
 infusiondb = [
   {
@@ -283,14 +278,14 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		var unitresults = initialisetests();
 		console.log(unitresults);
 
-		if (unitresults[0]=='fail') {
+		if (unitresults[0]=='pass') {
 			$("#teststatus").html("<b>Internal tests completed:</b><br>Last performed: " + unitresults[4] + "<br>Success on all unit tests during app launch. Drug dose calculation framework working as expected.<br><br>");
-		} else if (unitresults[0]=='pass') {
+		} else if (unitresults[0]=='fail') {
 			$("#teststatus").html("<b>Internal tests completed:</b><br>Last performed: " + unitresults[4] + "<br><b>FAILURE</b> on " + unitresults[2] + " of " + unitresults[1] + " unit tests during app launch.<br>Detailed test failure info: " + unitresults[3] + " <br><br>");
 			navigator.splashscreen.hide();
-			$("#detailedtestinfopanel").html("Time performed: " + unitresults[4] + "<br><b>FAILURE</b> on " + unitresults[2] + " of " + unitresults[1] + " unit tests during app launch.<br>Detailed test failure info: " + unitresults[3] + "<br><br>App version: " + appversion + "<br><br>");
+			$("#detailedtestinfopanel").html("Time performed: " + unitresults[4] + "<br><b>FAILURE</b> on " + unitresults[2] + " of " + unitresults[1] + " unit tests during app launch.<br><br>Detailed test failure info: <br>" + unitresults[3] + "<br><br>App version: " + appversion + "<br><br>");
 			$.mobile.changePage("#testfailure");
-			window.ga.trackException(unitresults[2] + "/" + unitresults[1] + "failed. V" + appversion + ". " + unitresults[3], Fatal);
+			window.ga.trackException(unitresults[2] + "/" + unitresults[1] + "failed. V" + appversion + ". " + unitresults[3], true);
 		}
 
 		if (navigator.userAgent.match(/Android/)) {
@@ -562,10 +557,11 @@ function unittest (actual, expected, comment) {
 		return true;
 	} else {
 		unittestfailcount=unittestfailcount+1;
+		commentdetail = '<b>ERROR:</b> ' + comment + ' (<b>expected:</b> ' + expected  + ', <b>observed:</b> ' + actual + ')';
 		if (unittestfailsummary == '') {
-			unittestfailsummary=comment;
+			unittestfailsummary=commentdetail;
 		} else {
-			unittestfailsummary=unittestfailsummary+', '+comment;			
+			unittestfailsummary=unittestfailsummary+', <br>'+commentdetail;
 		}
 		unittestsummary='fail';
 		return false;
@@ -579,18 +575,84 @@ function initialisetests() {
 	// we cannot predict.
 	// This should be run before the splash screen is cleared, so maximum
 	// 300ms or so to run this.
+	$("#babyweight").val(1000);
+	CalculateApp();
+	unittest( $("#babyweight").val(), 1000, "Weight input set to 1kg" );
+	unittest( $("#uvcdetails").text(), "Size: 3.5 FLength: 7 cm (plus stump length)L = (1.5 x birthweight [kg]) + 5.5", "UVC Size: 3.5 F, Length: 7 cm (plus stump length), L = (1.5 x birthweight [kg]) + 5.5" );
+	unittest( $("#uacdetails").text(), "Size: 3.5 FLength: 12 cm (plus stump length)L = (birthweight [kg] x 3) + 9", "UAC Size: 3.5 F, Length: 12 cm (plus stump length), L = (birthweight [kg] x 3) + 9" );  
+	unittest( $("#naclbolus").text(), "10 ml over 20-30 minutes, via IV/UVC", "Sodium chloride bolus 10 ml over 20-30 minutes" );
+	unittest( $("#dopamineinfusion").text(), "30 mg added to 50 ml of 0.9% NaCl or 5% dextrose.1 ml/hr will deliver 10 micrograms/kg/min.", "Dopamine 30 mg added to 50 ml of 0.9% NaCl or 5% dextrose. 1 ml/hr will deliver 10 micrograms/kg/min." );
+	unittest( $("#dobutamineinfusion").text(), "30 mg added to 50 ml of 0.9% NaCl or 5% dextrose.1 ml/hr will deliver 10 micrograms/kg/min. ", "Dobutamine infusion 30 mg added to 50 ml of 0.9% NaCl or 5% dextrose. 1 ml/hr will deliver 10 micrograms/kg/min. Max concentration 250mg in 50ml." );
+	unittest( $("#adrenalineinfusion").text(), "3 mg added to 50 ml of 0.9% NaCl or 5% dextrose.0.1 ml/hr will deliver 100 nanograms/kg/min.", "3 mg added to 50 ml of 0.9% NaCl or 5% dextrose.0.1 ml/hr will deliver 100 nanograms/kg/min." );
+	unittest( $("#noradrenalineinfusion").text(), "3 mg added to 50 ml of 0.9% NaCl or 5% dextrose.0.1 ml/hr will deliver 100 nanograms/kg/min.", "Noradrenaline infusion 3 mg added to 50 ml of 0.9% NaCl or 5% dextrose. 0.1 ml/hr will deliver 100 nanograms/kg/min." );
+	unittest( $("#protine2").text(), "15 micrograms added to 50 ml of 0.9% NaCl or 5% dextrose.1 ml/hr will deliver 5 nanograms/kg/min.", "Dinoprostone infusion 15 micrograms added to 50 ml of 0.9% NaCl or 5% dextrose. 1 ml/hr will deliver 5 nanograms/kg/min." );
+	unittest( $("#uvcdetails2").text(), "Size: 3.5 FLength: 7 cm (plus stump length)L = (1.5 x birthweight [kg]) + 5.5", "UVC details: Size: 3.5, F Length: 7 cm (plus stump length), L = (1.5 x birthweight [kg]) + 5.5" );
+	unittest( $("#uacdetails2").text(), "Size: 3.5 FLength: 12 cm (plus stump length)L = (birthweight [kg] x 3) + 9", "UAC details: Size: 3.5, F Length: 12 cm (plus stump length), L = (birthweight [kg] x 3) + 9" );
+	unittest( $("#fentanylbolus2").text(), "5 micrograms by slow IV push(preferred drug for intubation)", "Fentanyl bolus 5 micrograms by slow IV push (preferred drug for intubation)" );
+	unittest( $("#morphinebolus2").text(), "100 micrograms by slow IV push", "Morphine bolus 100 micrograms by slow IV push" );
+	unittest( $("#vecuroniumbolus").text(), "100 micrograms by slow IV push(for short duration of action)", "Vecuronium bolus 100 micrograms by slow IV push (for short duration of action)" );
+	unittest( $("#pancuroniumbolus").text(), "100 micrograms by slow IV push(for longer duration of action)", "Pancuronium bolus 100 micrograms by slow IV push (for longer duration of action)" );
+	unittest( $("#morphinesedativeinfusion").text(), "1 mg added to 50 ml of 0.9% NaCl or 5% Dextrose. 1 ml/hr will deliver 20 micrograms/kg/hr.", "1 mg added to 50 ml of 0.9% NaCl or 5% Dextrose. 1 ml/hr will deliver 20 micrograms/kg/hr." );
+	unittest( $("#vecuroniumsedativeinfusion").text(), "5 mg added to 50 ml of 0.9% NaCl or 5% Dextrose. 1 ml/hr will deliver 100 micrograms/kg/hr.", "5 mg added to 50 ml of 0.9% NaCl or 5% Dextrose. 1 ml/hr will deliver 100 micrograms/kg/hr." );
+	unittest( $("#phenobarbitoneloading").text(), "20 mg given IV over 20 minutes", "Phenobarbitone loading dose 20 mg given IV over 20 minutes" );
+	unittest( $("#phenytoinloading").text(), "18 mg given IV over 30 min(needs close ECG monitoring)", "Phenytoin loading dose 18 mg to be given over 30 min (needs close ECG monitoring)" );
+	unittest( $("#diazepam").text(), "300 micrograms(given IV over 5 min)", "Diazepam bolus 300 micrograms (IV injection over 5 min)" );
+	unittest( $("#midazolaminfusion").text(), "15 mg added to 50 ml 0.9% NaCl or 5% dextrose.0.1 ml/hr will deliver 30 micrograms/kg/hour.", "Midazolam infusion 15 mg added to 50 ml 0.9% NaCl or 5% dextrose. 0.1 ml/hr will deliver 30 micrograms/kg/hour." );
+	unittest( $("#adrenalineemergency").text(), "1st dose at 0.1 ml/kg= 0.1 ml IV stat.Flush with 0.9% sodium chloride.2nd dose at 0.3 ml/kg= 0.3 ml IV stat.Flush with 0.9% sodium chloride.", "Adrenaline stat dose 0.1/0.3 ml - correct" );
+	unittest( $("#dextrosebolus").text(), "2.5 ml over 20-30 minutes, via UVC or IV.", "Dextrose 2.5 ml over 20-30 mins - correct" );
+	unittest( $("#sodiumbicarb").text(), "2 ml (=1 mmol) of sodium bicarbonate 4.2% for use during emergency resuscitation, given via UVC", "Sodium bicarbonate emergency drug dose - correct" );
+	unittest( $("#adenosine").text(), "150 micrograms IV stat into a large central vein and followed immediately by NaCl 0.9% push.", "Adenosine dose 50mg IV stat - correct" );
+	unittest( $("#insulin").text(), "2 units added to 20 ml 5% dextrose.0.1 ml/hr will deliver 0.01 units/kg/hour, given IV as an infusion. Flush 5 ml through giving set before connecting.", "Insulin infusion details correct" );
+	unittest( $("#benpen").text(), "50 mg IV (BD/TDS depending on age)", "Benzylpenicillin dose 25mg/kg or 50mg/kg" );
+	unittest( $("#gentamicin").text(), "5 mg IV (frequency depending on post menstrual age)Check level before 2nd dose.", "5 mg 36 hourly IV.Check level before 2nd dose." );
+	unittest( $("#fluids").text(), "60 ml daily over 24 hours= 2.5 ml/hour infusion", "10% dextrose: 60 ml daily over 24 hours = 2.5 ml/hour infusion" );
+	unittest( $("#vitk").text(), "0.4 mg IM", "Vitamin K: 0.4 mg IM" );
+	$("#fluidsvalue").val("");
+	$("#dextrosefluid").val("10");
+	$("#tisodium").val("0");
+	$("#tipotassium").val("0");
+	unittest( $("#naclbolus2").text(), "10 ml over 20-30 minutes", "Sodium chloride 0.9% bolus: 10 ml over 20-30 minutes" );
+	unittest( $("#dextrosebolus2").text(), "2.5 ml over 20-30 minutes", "Dextrose 10% bolus: 2.5 ml over 20-30 minutes" );
+	$("#fluidsvalue").val("");
+	$("#dextrosefluid").val("10");
+	$("#tisodium").val("0");
+	$("#tipotassium").val("0");
+	$("#fluidsvalue").val(60);
+	CalculateApp();
+	unittest( $("#fluidsvalue").val(), 60, "Fluid input set to 60 ml/kg/day" );
+	unittest( $("#fluidoutput").text(), "Daily total fluid requirement= 60 ml/day, or 2.5 ml/hour.= 4.2 mg/kg/min glucose infusion rate PreparationTake a 500ml bag of 10% dextrose. ", "Daily total fluid requirement= 60 ml/day, or 2.5 ml/hour. Glucose infusion rate = 4.2 mg/kg/min. Preparation: Take a 500ml bag of 10% dextrose. " );
+	$("#fluidsvalue").val(60);
+	$("#tisodium").val(2);
+	$("#tipotassium").val(2);
+	CalculateApp();
+	unittest( $("#fluidsvalue").val(), 60, "Fluid input set to 60 ml/kg/day. 2mmol of sodium and potassium" );
+	unittest( $("#fluidoutput").text(), "Daily total fluid requirement= 60 ml/day, or 2.5 ml/hour.= 4.2 mg/kg/min glucose infusion rate PreparationTake a 500ml bag of 10% dextrose. Add:16.7 mmol sodium chloride(or add 1.3 mmol NaCl if using 10% dextrose + 0.18% saline, as a bag already contains 15.4 mmol sodium)16.7 mmol potassium chloride", "Fluids with 2mmol of sodium/potassium: Daily total fluid requirement= 60 ml/day, or 2.5 ml/hour. Glucose infusion rate = 4.2 mg/kg/min. Take a 500ml bag of 10% dextrose. Add 16.7 mmol sodium chloride (or add 1.3 mmol NaCl if using 10% dextrose + 0.18% saline, as a bag already contains 15.4 mmol sodium), add 16.7 mmol potassium chloride." );
+	$("#fluidsvalue").val(90);
+	$("#dextrosefluid").val(15);
+	$("#tisodium").val(0);
+	$("#tipotassium").val(0);
+	CalculateApp();
+	unittest( $("#fluidsvalue").val(), 90, "Fluid input set to 90 ml/kg/day. Dextrose concentration set to 15%. No sodium or potassium." );
+	unittest( $("#fluidoutput").text(), "Daily total fluid requirement= 90 ml/day, or 3.8 ml/hour.= 9.5 mg/kg/min glucose infusion rate Administration routeInfuse via central line (dextrose concentration > 12.5%).PreparationTake a 500ml bag of 10% dextrose, remove 62.5 ml and add 62.5 ml of 50% dextrose. ","Dextrose 15%: Daily total fluid requirement= 90 ml/day, or 3.8 ml/hour. Glucose infusion rate = 9.5 mg/kg/min. Administration route: Infuse via central line (dextrose concentration > 12.5%). PreparationTake a 500ml bag of 10% dextrose, remove 62.5 ml and add 62.5 ml of 50% dextrose." );
+	// Check infusion calculator
+	// Dopamine as example
+	var inputdrugname="dopamine";
+	var drug=infusiondb.filter(function (search) { return search.name == inputdrugname })[0];
+	var drugname=inputdrugname;
+	var infusion = new Infusion(drug.name, 10, drug.units, drug.desiredrate, 1000, drug.syringe, drug.fluidtype, 0, drug.dosemsg, drug.minconc, drug.maxconc, drug.minconcmsg, drug.maxconcmsg, drug.mindose, drug.maxdose, drug.units);
+	unittest(infusion.outputtext(),"30 mg added to 50 ml of 0.9% NaCl or 5% dextrose. <br>1 ml/hr will deliver 10 micrograms/kg/min.","Dopamine infusion calc: outputtext()");
+	unittest(infusion.maxconcmsg,"Concentrations higher than 1.6 mg/ml are not recommended, unless infused centrally (BNFc 2015).","Dopamine infusion calc: maxconcmsg true");
 
-	// 1. Complete check of intubation
-		$("#babyweight").val(1000);
-		CalculateApp();
-		unittest( $("#babyweight").val(), 1000, "Weight input set to 1kg" );
-		unittest( $("#ettsize").text(), "3.0", "ETT = 3.0" );
-		unittest( $("#ettlength").text(), "6.5 cm", "ETT = Tube length = 6.5cm" );
-		unittest( $("#fentanylbolus").text(), "5 micrograms by slow IV push(preferred drug for intubation)", "Fentanyl: 5 micrograms by slow IV push(preferred drug for intubation)" );
-		unittest( $("#morphinebolus").text(), "100 micrograms by slow IV push(nb onset of action approx 5 mins; peak action at 15 - 30 mins)", "Morphine: 100 micrograms by slow IV push(effective only after 20-30 mins)" );
-		unittest( $("#suxbolus").text(), "2 mg by slow IV push", "Suxamethonium 2 mg by slow IV push" );
-		unittest( $("#atropinebolus").text(), "20 micrograms by slow IV push", "Atropine 20 micrograms by slow IV push" );
-		unittest( $("#curosurf").text(), "First dose: 200 mg,Second dose: 100 mg (if indicated)Give via ET tube.Practical note: use whole vial closest to calculated dose.", "First dose: 200 mg,Second dose: 100 mg (if indicated)Give via ET tube.Practical note: use whole vial closest to calculated dose." );
+	// High concentration dopamine
+	var infusion = new Infusion(drug.name, 10, drug.units, 0.1, 1000, drug.syringe, drug.fluidtype, 0, drug.dosemsg, drug.minconc, drug.maxconc, drug.minconcmsg, drug.maxconcmsg, drug.mindose, drug.maxdose, drug.units);
+	unittest(infusion.advisorymsg,"Infusion concentration is 6 mg/ml. Concentrations higher than 1.6 mg/ml are not recommended, unless infused centrally (BNFc 2015).","Dopamine high concentration warning given.");
+
+	// Adrenaline as another example
+	var inputdrugname="adrenaline";
+	var drug=infusiondb.filter(function (search) { return search.name == inputdrugname })[0];
+	var drugname=inputdrugname;
+	var infusion = new Infusion(drug.name, 100, drug.units, drug.desiredrate, 1000, drug.syringe, drug.fluidtype, 0, drug.dosemsg, drug.minconc, drug.maxconc, drug.minconcmsg, drug.maxconcmsg, drug.mindose, drug.maxdose, drug.units);
+	unittest(infusion.outputtext(),"3 mg added to 50 ml of 0.9% NaCl or 5% dextrose. <br>0.1 ml/hr will deliver 100 nanograms/kg/min.","Adrenaline infusion calc: outputtext()");
 
 	// Reset things back to initalised
 	$("#babyweight").val('');
