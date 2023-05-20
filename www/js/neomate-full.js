f6243c202f9322e76070af3b4bf393c1cf01fa75
Christopher Kelly <chris@incubate.co.uk>
2023-05-20 23:27:47 +0100

    v3.2 commit - Ionic

--- www/js/neomate-full.js
+++ www/js/neomate-full.js
@@ -741,7 +741,7 @@ function initialisetests() {
 	unittest( $("#phenytoinloading").text(), "18 mg given IV over 30 min(needs close ECG monitoring)", "Phenytoin loading dose 18 mg to be given over 30 min (needs close ECG monitoring)" );
 	unittest( $("#diazepam").text(), "300 micrograms(given IV over 5 min)", "Diazepam bolus 300 micrograms (IV injection over 5 min)" );
 	unittest( $("#midazolaminfusion").text(), "15 mg added to 50 ml 0.9% NaCl or 5% dextrose.0.1 ml/hr will deliver 30 micrograms/kg/hour.", "Midazolam infusion 15 mg added to 50 ml 0.9% NaCl or 5% dextrose. 0.1 ml/hr will deliver 30 micrograms/kg/hour." );
-	unittest( $("#adrenalineemergency").text(), "1st dose at 0.1 ml/kg= 0.1 ml IV stat.Flush with 0.9% sodium chloride.2nd dose at 0.3 ml/kg= 0.3 ml IV stat.Flush with 0.9% sodium chloride.", "Adrenaline stat dose 0.1/0.3 ml - correct" );
+	unittest( $("#adrenalineemergency").text(), "0.2 ml/kg= 0.2 ml IV stat.Flush with 0.9% sodium chloride.", "Adrenaline dose 0.2 ml - correct" );
 	unittest( $("#dextrosebolus").text(), "2.5 ml over 20-30 minutes, via UVC or IV.", "Dextrose 2.5 ml over 20-30 mins - correct" );
 	unittest( $("#sodiumbicarb").text(), "2 ml (=1 mmol) of sodium bicarbonate 4.2% for use during emergency resuscitation, given via UVC", "Sodium bicarbonate emergency drug dose - correct" );
 	unittest( $("#adenosine").text(), "150 micrograms IV stat into a large central vein and followed immediately by NaCl 0.9% push.", "Adenosine dose 50mg IV stat - correct" );
@@ -1538,25 +1538,25 @@ PREVIOUS SIZES
 
 
 	// EMERGENCY DRUGS
-		var adrenalinedose1 = Math.round((babyweight*0.1)*10)/10;
-		var adrenalinedose2 = Math.round((babyweight*0.3)*10)/10;
+		var adrenalinedose1 = Math.round((babyweight*0.2)*10)/10;
+//		var adrenalinedose2 = Math.round((babyweight*0.3)*10)/10;
 		var adrenalinemsg = "";
-		var adrenalinemsg2 = "";
+//		var adrenalinemsg2 = "";
 		if (adrenalinedose1<0.1) {
 			adrenalinedose1 = 0.1;
 			adrenalinemsg = "<br>(minimum 0.1 ml)";
 		}
-		if (adrenalinedose2<0.1) {
-			adrenalinedose2 = 0.1;
-			adrenalinemsg2 = "<br>(minimum 0.1 ml)";
-		}
+//		if (adrenalinedose2<0.1) {
+//			adrenalinedose2 = 0.1;
+//			adrenalinemsg2 = "<br>(minimum 0.1 ml)";
+//		}
 		if (babyweight > 1.5) {
 			var fcorrection = 0.4;
 		} else {
 			var fcorrection = 0.5;
 		}
 
-		$("#adrenalineemergency").html("<b>1st dose</b> at 0.1 ml/kg<br>= " + adrenalinedose1 + " ml IV stat." + adrenalinemsg + "<br>Flush with 0.9% sodium chloride.<br><br><b>2nd dose</b> at 0.3 ml/kg<br>= " + adrenalinedose2 + " ml IV stat." + adrenalinemsg2 + "<br>Flush with 0.9% sodium chloride.");
+		$("#adrenalineemergency").html("0.2 ml/kg<br>= " + adrenalinedose1 + " ml IV stat." + adrenalinemsg + "<br>Flush with 0.9% sodium chloride.<br><br>");
 		$("#sodiumbicarb").html((Math.round((babyweight*2)*10)/10) + " ml (=" + (Math.round((babyweight*2/2)*10)/10) + " mmol) of sodium bicarbonate <span class=\"highlightdrug\">4.2%</span> for use during emergency resuscitation, given via UVC");
 		$("#sodiumbicarbhalfcorrection").html("<a href=\"#halfcorrection\" data-transition=\"none\" data-corners=\"false\" style=\"text-decoration: none; color: rgb(52, 73, 94)\">Base deficit (mmol/L) x " + fcorrection + " x [" + (Math.round((babyweight)*10)/10) +" kg (weight)]. Only <b>half</b> the base deficit should be corrected initially by slow IV infusion (dilute sodium bicarbonate 4.2% with equal parts water and give over 30 minutes).</a>");
 		$("#dextrosebolus").text((Math.round((babyweight*2.5)*10)/10) + " ml over 20-30 minutes, via UVC or IV.") ;
