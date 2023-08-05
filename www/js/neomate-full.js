7f534c42fdf9a337a23986bd324836dea8959db3
Christopher Kelly <chris@incubate.co.uk>
2023-08-05 22:53:25 +0100

    Adding Firebase plugin - cont

--- www/js/neomate-full.js
+++ www/js/neomate-full.js
@@ -362,7 +362,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 			navigator.splashscreen.hide();
 			$("#detailedtestinfopanel").html("Time performed: " + unitresults[4] + "<br><b>FAILURE</b> on " + unitresults[2] + " of " + unitresults[1] + " unit tests during app launch.<br><br>Detailed test failure info: <br>" + unitresults[3] + "<br><br>App version: " + appversion + "<br><br>");
 			$.mobile.changePage("#testfailure");
-			window.ga.trackException(unitresults[2] + "/" + unitresults[1] + " failed. V" + appversion + ". " + unitresults[3], true);
+			// window.ga.trackException(unitresults[2] + "/" + unitresults[1] + " failed. V" + appversion + ". " + unitresults[3], true);
+			trackPage("InitialisationTestFailure")
 		}
 
 		if (navigator.userAgent.match(/Android/)) {
